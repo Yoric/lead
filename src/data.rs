40caade0194749d4a6724ba6e8d30d9d27c0a779
David Teller <d.o.teller+github@gmail.com>
2023-02-25 13:47:11 +0100

    We can now add leads and take notes

--- src/data.rs
+++ src/data.rs
@@ -1,41 +1,169 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
+use anyhow::{anyhow, Context};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Hash, PartialEq, Eq, Deserialize, Serialize)]
-pub struct LeadName(String);
+/// A lead e.g. a company.
+#[derive(clap::Args, Clone, Debug, Hash, PartialEq, Eq, Deserialize, Serialize)]
+#[serde(transparent)]
+pub struct CompanyName {
+    name: Arc<str>,
+}
+impl From<String> for CompanyName {
+    fn from(name: String) -> Self {
+        Self { name: name.into() }
+    }
+}
 
-#[derive(Debug, Hash, PartialEq, Eq, Deserialize, Serialize)]
-pub struct InterviewName(String);
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Deserialize, Serialize)]
+#[serde(transparent)]
+pub struct InterviewName {
+    name: Arc<str>,
+}
+impl From<String> for InterviewName {
+    fn from(name: String) -> Self {
+        Self { name: name.into() }
+    }
+}
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Data {
-    /// All our leads, opened or closed keyed by
-    pub leads: HashMap<LeadName, Lead>,
+pub struct Leads {
+    /// All our leads, indexed by the company name.
+    #[serde(flatten)]
+    pub leads: HashMap<CompanyName, Vec<Lead>>,
 }
-impl Data {
-    pub fn new_lead(&mut self, name: String) -> Result<Lead, anyhow::Error> {
-        unimplemented!()
+impl Leads {
+    pub fn new() -> Self {
+        Self {
+            leads: HashMap::new(),
+        }
     }
-    pub fn lead(&self, name: &str) -> Result<&Lead, Vec<&str>> {
-        unimplemented!()
+    pub fn from_path(path: &std::path::Path) -> Result<Self, anyhow::Error> {
+        let db = match std::fs::File::open(path) {
+            // Reuse file if possible.
+            Ok(db_file) => serde_yaml::from_reader(&db_file)
+                .with_context(|| format!("Invalid yaml file {}", path.display()))?,
+
+            // Create the file if it doesn't exist.
+            Err(ref err) if matches!(err.kind(), std::io::ErrorKind::NotFound) => Leads::new(),
+
+            // Otherwise, propagate error.
+            Err(err) => {
+                return Err(err).with_context(|| {
+                    format!("Error while reading file {path}", path = path.display())
+                })
+            }
+        };
+        Ok(db)
     }
-    pub fn lead_mut(&mut self, name: &str) -> Result<&mut Lead, Vec<&str>> {
-        unimplemented!()
+    pub fn new_lead(&mut self, name: CompanyName, position: String, source: String) -> usize {
+        let lead = Lead::new(position, source);
+        self.push_lead(name, lead)
+    }
+    pub fn push_lead(&mut self, name: CompanyName, lead: Lead) -> usize {
+        let positions = self.leads.entry(name).or_default();
+        positions.push(lead);
+        positions.len() - 1
+    }
+    pub fn close_lead(
+        &mut self,
+        name: &CompanyName,
+        index: Option<usize>,
+        reason: String,
+    ) -> Result<Lead, anyhow::Error> {
+        let positions = self.leads.get_mut(name).context("No such company")?;
+        let mut lead = match index {
+            None if positions.len() == 1 => positions.pop().unwrap(),
+            None => {
+                return Err(anyhow!(
+                    "There are {} positions for this company, please specify which one to close",
+                    positions.len()
+                ))
+            }
+            Some(index) if index < positions.len() => positions.remove(index),
+            Some(index) => {
+                return Err(anyhow!(
+                    "There are only {} positions for this company, cannot close position {}",
+                    positions.len(),
+                    index
+                ))
+            }
+        };
+        lead.closed = Some(reason);
+        Ok(lead)
+    }
+    pub fn get_mut(
+        &mut self,
+        name: &CompanyName,
+        index: Option<usize>,
+    ) -> Result<&mut Lead, anyhow::Error> {
+        let positions = self.leads.get_mut(name).context("No such company")?;
+        let lead = match index {
+            None if positions.len() == 1 => &mut positions[0],
+            None => {
+                return Err(anyhow!(
+                    "There are {} positions for this company, please specify which one to modify",
+                    positions.len()
+                ))
+            }
+            Some(index) if index < positions.len() => &mut positions[index],
+            Some(index) => {
+                return Err(anyhow!(
+                    "There are only {} positions for this company, cannot modify position {}",
+                    positions.len(),
+                    index
+                ))
+            }
+        };
+        Ok(lead)
     }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Lead {
+    /// The name of the position.
+    position: String,
+
+    /// The source of the lead, typically a URL.
+    source: String,
+
+    #[serde(default)]
+    notes: HashMap<String, Vec<String>>,
+
+    #[serde(default)]
     interviews: Vec<(InterviewName, Interview)>,
+
+    #[serde(default)]
     red_flags: Vec<String>,
 
     /// The status updates, from oldest to most recent.
+    #[serde(default)]
     status_updates: Vec<String>,
+
+    /// The reason for which this lead was closed, if any.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    closed: Option<String>,
+}
+
+impl Lead {
+    pub fn new(position: String, source: String) -> Self {
+        Self {
+            position,
+            source,
+            interviews: Vec::new(),
+            red_flags: Vec::new(),
+            status_updates: Vec::new(),
+            closed: None,
+            notes: HashMap::new(),
+        }
+    }
+    pub fn add_note(&mut self, name: String, note: String) {
+        self.notes.entry(name).or_default().push(note);
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Interview {
-    pre_notes:  Vec<String>,
+    pre_notes: Vec<String>,
     post_notes: Vec<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,163 @@
-use chrono::{ DateTime, Utc };
-use data::LeadName;
+use std::path::PathBuf;
+
+use anyhow::Context;
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use data::CompanyName;
 
 mod data;
 
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// The name of the company.
+    company: CompanyName,
+
+    /// If there is more than one position at the company, the index of the position.
+    index: Option<usize>,
+
+    #[command(subcommand)]
+    command: Command,
+
+    #[arg(default_value_t=Utc::now())]
+    updated_on: DateTime<Utc>,
+
+    #[arg(long, default_value_t={"leads.yml".to_string()})]
+    file: String,
+
+    #[arg(long, default_value_t={"archive.yml".to_string()})]
+    archive: String,
+}
+
+#[derive(clap::Subcommand, Debug)]
 enum Command {
     /// Create a new lead.
     New {
-        lead: LeadName,
-        updated_on: Option<DateTime<Utc>>
+        /// The name of the position.
+        position: String,
+
+        /// The source for this lead, typically a URL.
+        source: String,
     },
 
     /// Close a lead.
     Close {
-        lead: LeadName,
         reason: String,
-        updated_on: Option<DateTime<Utc>>
+    },
+
+    /// Misc note.
+    #[command(subcommand)]
+    Note(NoteCommand),
+
+    /// Things to prepare for a lead
+    ToDo {
+        /// A description of the task.
+        description: String,
     },
 
     /// Things to know about an upcoming interview.
     PreInterview {
-        lead: LeadName,
-
         /// The name or stage of the interview.
         interview: String,
         notes: String,
-        updated_on: Option<DateTime<Utc>>,
-        planned: Option<DateTime<Utc>>
+
+        /// The date the interview is planned for.
+        planned: Option<DateTime<Utc>>,
     },
 
     /// Things to know after an incoming interview.
     PostInterview {
-        lead: LeadName,
-
         /// The name or stage of the interview.
         interview: String,
         notes: String,
         held_on: DateTime<Utc>,
-        updated_on: Option<DateTime<Utc>>,
     },
 
     RedFlag {
-        lead: LeadName,
         comment: String,
-        updated_on: Option<DateTime<Utc>>,
     },
 
     /// Update
     Status {
-        lead: LeadName,
         status: String,
-        updated_on: Option<DateTime<Utc>>,
-    },
-
-    Detail {
-        lead: LeadName,
-        /// The kind of detail, e.g. "Open to remote", "Salary", etc.
-        kind: String,
-        updated_on: Option<DateTime<Utc>>,
-    },
-
-    /// Misc note.
-    Note {
-        name: String,
-        note: String,
-        updated_on: Option<DateTime<Utc>>,
     },
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(clap::Subcommand, Debug)]
+enum NoteCommand {
+    /// Create a new note.
+    Add {
+        name: String,
+        note: String,
+    },
+    Replace {
+        name: String,
+        note: String,
+    },
+}
+
+impl Args {
+    pub fn execute(self, db: &mut data::Leads) -> Result<(), anyhow::Error> {
+        match self.command {
+            Command::New { position, source } => {
+                if self.index.is_some() {
+                    return Err(anyhow::anyhow!(
+                        "Cannot specify index when creating a new lead"
+                    ));
+                }
+                db.new_lead(self.company, position, source);
+            }
+            Command::Close { reason } => {
+                let lead = db
+                    .close_lead(&self.company, self.index, reason)
+                    .context("Failed to remove lead")?;
+                let db_archive_path = PathBuf::from(&self.archive);
+                let mut db_archive = data::Leads::from_path(&db_archive_path)
+                    .context("Failed to load or create archive")?;
+                db_archive.push_lead(self.company, lead);
+                serde_yaml::to_writer(std::fs::File::create(&db_archive_path)?, &db_archive)
+                    .context("Failed to write archive")?;
+            }
+            Command::Note(NoteCommand::Add { name, note }) => {
+                let lead = db
+                    .get_mut(&self.company, self.index)
+                    .context("Failed to get lead")?;
+                lead.add_note(name, note);
+            }
+
+            Command::Note(NoteCommand::Replace { .. }) => {
+                unimplemented!()
+            }
+
+            Command::ToDo { .. } => {
+                unimplemented!()
+            }
+            Command::PreInterview { .. } => {
+                unimplemented!()
+            }
+            Command::PostInterview { .. } => {
+                unimplemented!()
+            }
+            Command::RedFlag { .. } => {
+                unimplemented!()
+            }
+            Command::Status { .. } => {
+                unimplemented!()
+            }
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+
+    // Load db
+    let db_path = PathBuf::from(&args.file);
+    let mut db = data::Leads::from_path(&db_path)?;
+    args.execute(&mut db)?;
+
+    // Write back to disk.
+    serde_yaml::to_writer(std::fs::File::create(&db_path)?, &db)?;
+    Ok(())
 }
